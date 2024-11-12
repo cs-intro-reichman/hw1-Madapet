acf867114a8f6c808a8e04b48afdf282acf3bc4e
adam <mada.carpm>
2024-11-12 14:15:25 +0200

    bill3

--- Bill3.java
+++ Bill3.java
@@ -7,14 +7,12 @@ public class Bill3 {
 		
 		// To get you started, here is the first line in the program:
 
-	    String name1 = args[0]; // Gets from the user the first name 
-	    String name2 = args[1]; // Gets from the user the secound name 
-	    String name3 = args[2]; // Gets from the user the third name 
+	   
 		int bill = Integer.parseInt(args[3]); // Get the bill 
 		bill = (int) Math.ceil(bill / 3.0);
-		System.err.println("Dear " +name3 +", "+ name2 +", and " + name1 +": pay " + (double)bill +" Shekels each" ); // Print
-		
-		
+	
+		System.err.println("Dear " +args[2] +", "+ args[1] +", and " + 
+		args[0] +": pay " + (double)bill +" Shekels each" ); 
 
 	}
 }
