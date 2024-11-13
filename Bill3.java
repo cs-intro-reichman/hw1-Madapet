281d6485990e6a8cf8cf6353bb057e5b4eb8444b
adam <mada.carpm>
2024-11-13 13:18:06 +0200

    Bill3

--- Bill3.java
+++ Bill3.java
@@ -5,11 +5,10 @@ public class Bill3 {
 	public static void main(String[] args) {
 		
 		// To get you started, here is the first line in the program:
-		System.err.println("");
+		System.err.println("123");
 
 	   
 		int bill = Integer.parseInt(args[3]); // Get the bill 
-		bill = (int) Math.ceil(bill / 3.0);
 	
 		System.err.println("Dear " +args[2] +", "+ args[1] +", and " + 
 		args[0] +": pay " + (double)bill +" Shekels each" ); 
